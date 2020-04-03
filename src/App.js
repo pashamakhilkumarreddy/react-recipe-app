6f1a3183a60c63a98aaf273a9722e1bce471bded
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-04-03 22:12:35 +0530

    Add Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Routes from './routes';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <main className="container">
+        <Routes />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+export default () =>
+  <Switch>
+    
+  </Switch>
